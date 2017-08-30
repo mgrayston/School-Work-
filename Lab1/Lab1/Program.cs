e94de97c5da9fbf952a3b1af7bc4d12a29b1e47a
Christopher Nielson <cnielson3@gmail.com>
2017-08-30 12:00:01 -0600

    Added Lab1 solution and Main hello world method.

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1 {
+    class Program {
+        /// <summary>
+        /// Prints "Hello, World!" to console.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
