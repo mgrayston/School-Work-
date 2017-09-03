e566f59b28f9f58cecb148f3e7925657041a0b65
Christopher Nielson <cnielson3@gmail.com>
2017-09-02 21:43:48 -0600

    Restructured the control flow of adding tokens to stacks; added comments on things to be done.

--- PS1/FormulaEvaluator/Class1.cs
+++ PS1/FormulaEvaluator/Class1.cs
@@ -10,54 +10,54 @@ namespace FormulaEvaluator {
 
         public delegate int Lookup(String v);
 
+        /// <summary>
+        /// Evaluates the result of a given arithmetic expression. Returns the result.
+        /// </summary>
+        /// <param name="exp">The arithmetic expression to be evaluated.</param>
+        /// <param name="variableEvaluator">Lookup used to retrieve stored values of variables in the expression.</param>
+        /// <returns>Result of exp.</returns>
         public static int Evaluate(String exp, Lookup variableEvaluator) {
+            int result = -999;
+
             string[] tokens = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
-            String regexOps = "\\+|\\-|\\*|\\/|\\(|\\)";
-            String regexParens = "\\(|\\)";
 
             Stack<String> values = new Stack<String>();
             Stack<String> operators = new Stack<String>();
 
-            foreach (int token in Enumerable.Range(0,tokens.Length)) {
-                // continue to next token if it is empty
-                if (tokens[token] != null) {
-                    // eliminate extra whitespace
-                    Regex.Replace(tokens[token], "\\s+", "");
-                    // continue to next token if nothing remains
-                    if (token == null) {
-                        continue;
+            // push all tokens onto stacks
+            foreach (int token in Enumerable.Range(0, tokens.Length)) {
+                // eliminate extra whitespace
+                Regex.Replace(tokens[token], "\\s+", "");
+
+                if (!tokens[token].Equals("")) {
+                    // throw exception for invalid variable name
+                    if (Regex.IsMatch(tokens[token], "\\d+[a-zA-Z]+")) {    // add for ONLY a letter (a-zA-Z anchored at beginning and end
+                        throw new ArgumentException();
                     }
 
-                    // token is a value
-                    if (!Regex.IsMatch(tokens[token], regexOps)) {
-                        // invalid variable
-                        if (Regex.IsMatch(tokens[token], "\\d+[a-zA-Z]+")) {
-                            throw new ArgumentException();
-                        }
+                    // '+' and '-' operators
+                    else if (Regex.IsMatch(tokens[token], "\\+|\\-")) {
+                        // TODO
+                    }
 
-                        // if last token was not an operator, throw exception
-                        if (token > 0 && !Regex.IsMatch(tokens[token - 1], regexOps)) {
-                            throw new ArgumentException();
-                        }
+                    // '*' and '/' operators
+                    else if (Regex.IsMatch(tokens[token], "\\/|\\*")) {
+                        // TODO
+                    }
 
-                        // push valid value
-                        values.Push(tokens[token]);
-                    } else {
-                        // token is an operator
+                    // variables
+                    else if (Regex.IsMatch(tokens[token], "[a-zA-Z]+\\d+")) {   // add rule for there not being anything before or after
+                        // TODO
+                    }
 
-                        // current token is not a parenthesis and last token is an operator other than a parenthesis
-                        if (!Regex.IsMatch(tokens[token], regexParens) && Regex.IsMatch(tokens[token - 1], regexOps) 
-                            && !Regex.IsMatch(tokens[token - 1], regexParens)) {
-                            throw new ArgumentException();
-                        }
-
-                        // push valid operator
-                        operators.Push(tokens[token]);
+                    // digits
+                    else if (Regex.IsMatch(tokens[token], "\\d+")) {    // add rule for nothing before or after
+                        // TODO
                     }
                 }
             }
 
-            return -1;
+            return result;
         }
     }
 }
