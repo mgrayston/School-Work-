c6713cb6f60fb1935c2d0c510d70c9aae7eb11a1
Christopher Nielson <cnielson3@gmail.com>
2017-09-20 12:03:55 -0600

    Fixed Structure

--- PS3/Formula/Properties/AssemblyInfo.cs
+++ PS3/Formula/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PS3")]
+[assembly: AssemblyTitle("Formula")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PS3")]
+[assembly: AssemblyProduct("Formula")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2829a14c-4969-4020-bdf8-f54f63de8f6b")]
+[assembly: Guid("70e2a861-1cc2-421e-a951-33330855ffd6")]
 
 // Version information for an assembly consists of the following four values:
 //
