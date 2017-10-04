a7f1fa25f5ccbd7a11dfdcf0210bee2614a2ffda
Christopher Nielson <cnielson3@gmail.com>
2017-10-04 12:48:24 -0600

    Started to flesh out PS5 skeleton code. Replaced AbstratSpreadsheet

--- PS4/Spreadsheet/Spreadsheet.cs
+++ PS4/Spreadsheet/Spreadsheet.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// TODO replace all names with a normalized version
+
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using SpreadsheetUtilities;
@@ -10,11 +12,16 @@ namespace SS {
         private Dictionary<string, object> contents;    // Dictionary that acts as the "spreadsheet", storing all cells and their contents
         private Dictionary<string, object> values;      // Used to store values of cells
         private DependencyGraph cellGraph;              // Used to track dependencies of all cells
+        private Func<string, bool> isValid;             // Used to test the validity of variables
+        private Func<string, string> normalize;         // Used to normalize variables
+        private string version;                         // Used to track version information of a Spreadsheet
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Spreadsheet"/> class.
-        /// </summary>
-        public Spreadsheet() {
+        public override bool Changed { get => throw new NotImplementedException(); protected set => throw new NotImplementedException(); }
+
+        public Spreadsheet(Func<string, bool> isValid, Func<string, string> normalize, string version) : base(isValid, normalize, version) {
+            this.isValid = isValid;
+            this.normalize = normalize;
+            this.version = version;
             contents = new Dictionary<string, object>();
             values = new Dictionary<string, object>();
             cellGraph = new DependencyGraph();
@@ -35,7 +42,8 @@ namespace SS {
         /// <returns></returns>
         /// <exception cref="InvalidNameException"></exception>
         public override object GetCellContents(string name) {
-            if (isValidName(name)) {
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
                 if (contents.ContainsKey(name) && contents[name] != null) {
                     return contents[name];
                 }
@@ -45,6 +53,50 @@ namespace SS {
             }
 
             throw new InvalidNameException();
+        }
+
+        /// <summary>
+        /// If content is null, throws an ArgumentNullException.
+        /// 
+        /// Otherwise, if name is null or invalid, throws an InvalidNameException.
+        /// 
+        /// Otherwise, if content parses as a double, the contents of the named
+        /// cell becomes that double.
+        /// 
+        /// Otherwise, if content begins with the character '=', an attempt is made
+        /// to parse the remainder of content into a Formula f using the Formula
+        /// constructor.  There are then three possibilities:
+        /// 
+        ///   (1) If the remainder of content cannot be parsed into a Formula, a 
+        ///       SpreadsheetUtilities.FormulaFormatException is thrown.
+        ///       
+        ///   (2) Otherwise, if changing the contents of the named cell to be f
+        ///       would cause a circular dependency, a CircularException is thrown.
+        ///       
+        ///   (3) Otherwise, the contents of the named cell becomes f.
+        /// 
+        /// Otherwise, the contents of the named cell becomes content.
+        /// 
+        /// If an exception is not thrown, the method returns a set consisting of
+        /// name plus the names of all other cells whose value depends, directly
+        /// or indirectly, on the named cell.
+        /// 
+        /// For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        /// </summary>
+        public abstract ISet<String> SetContentsOfCell(String name, String content) {
+            if (content == null) {
+                throw new ArgumentNullException("No content passed");
+            }
+
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
+
+                // TODO
+            }
+            else {
+                throw new InvalidNameException(name + " is not a valid name");
+            }
         }
 
         /// <summary>
@@ -59,8 +111,9 @@ namespace SS {
         /// <param name="number"></param>
         /// <returns></returns>
         /// <exception cref="InvalidNameException"></exception>
-        public override ISet<string> SetCellContents(string name, double number) {
-            if (isValidName(name)) {
+        protected override ISet<string> SetCellContents(string name, double number) {
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
                 contents[name] = number;
                 values[name] = number;
                 cellGraph.ReplaceDependees(name, null);
@@ -84,12 +137,13 @@ namespace SS {
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidNameException"></exception>
-        public override ISet<string> SetCellContents(string name, string text) {
+        protected override ISet<string> SetCellContents(string name, string text) {
             if (text == null) {
                 throw new ArgumentNullException(text);
             }
 
-            if (isValidName(name)) {
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
                 contents[name] = text;
                 values[name] = text;
                 cellGraph.ReplaceDependees(name, null);
@@ -115,19 +169,20 @@ namespace SS {
         /// <returns></returns>
         /// <exception cref="ArgumentNullException">The provided formula was null</exception>
         /// <exception cref="InvalidNameException"></exception>
-        public override ISet<string> SetCellContents(string name, Formula formula) {
+        protected override ISet<string> SetCellContents(string name, Formula formula) {
             if (formula == null) {
                 throw new ArgumentNullException("The provided formula was null");
             }
 
-            if (isValidName(name)) {
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
                 // Get variables in formula
                 IEnumerable<string> vars = formula.GetVariables();
                 // Get all direct/indirect dependents of the formula
                 HashSet<string> changed = new HashSet<string>(GetCellsToRecalculate(new HashSet<string>(vars)));
                 // Ensure the formula is not circular
                 contents[name] = formula;
-                values[name] = formula.Evaluate(getValue);
+                values[name] = formula.Evaluate(GetCellValue);
                 cellGraph.ReplaceDependees(name, vars);
                 // CircularException will be thrown by GetCelsToRecalculate if needed
                 return new HashSet<string>(GetCellsToRecalculate(name));
@@ -159,7 +214,8 @@ namespace SS {
                 throw new ArgumentNullException(name);
             }
 
-            if (isValidName(name)) {
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
                 return cellGraph.GetDependents(name);
             }
 
@@ -177,7 +233,9 @@ namespace SS {
             if (name != null) {
                 if (char.IsLetter(name[0]) || name[0] == '_') {
                     if (name.Length == 1 || !Regex.IsMatch(name.Substring(1), "[^\\d^\\w^_]")) {
-                        return true;
+                        if (isValid(name)) {
+                            return true;
+                        }
                     }
                 }
             }
@@ -185,6 +243,7 @@ namespace SS {
             return false;
         }
 
+        /*
         /// <summary>
         /// Gets the value of the given variable.
         /// Used as a delegate for Formula.evaluate to calculate a cell's value.
@@ -199,6 +258,32 @@ namespace SS {
             else {
                 throw new FormatException("Variable " + var + " not found");
             }
+        }
+        // TODO deprecated
+        */
+
+        public override string GetSavedVersion(string filename) {
+            throw new NotImplementedException();
+        }
+
+        public override void Save(string filename) {
+            throw new NotImplementedException();
+        }
+
+        public override object GetCellValue(string name) {
+            if (isValidName(normalize(name))) {
+                name = normalize(name);
+
+                if (values[name] is double) {
+                    return (double)values[name];
+                }
+            }
+
+            throw new FormatException("Variable " + name + " not found");
+        }
+
+        public override ISet<string> SetContentsOfCell(string name, string content) {
+            throw new NotImplementedException();
         }
     }
 }
