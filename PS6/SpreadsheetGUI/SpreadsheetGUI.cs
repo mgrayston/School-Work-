6e10cd1975140eb181c06a0de8fc064c37130292
kirusenn <cnielson3@gmail.com>
2017-10-22 17:29:28 -0600

    GUI now creates a new Spreadsheet when it is made.

--- PS6/SpreadsheetGUI/SpreadsheetGUI.cs
+++ PS6/SpreadsheetGUI/SpreadsheetGUI.cs
@@ -7,10 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SS;
 
 namespace SpreadsheetGUI {
     public partial class SpreadsheetGUI : Form {
+
+        private Spreadsheet ss; // Backing Spreadsheet for this GUI
+
         public SpreadsheetGUI() {
+            ss = new Spreadsheet(s => true, s => s, "ps6");
             InitializeComponent();
         }
     }
