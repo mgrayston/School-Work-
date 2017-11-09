aee346560df3b4b3a35d244233349eb571173f7b
Christopher Nielson <cnielson3@gmail.com>
2017-11-09 15:21:00 -0700

    Started adding NetworkCode

---
+++ SpaceWars/Controller/NetworkController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkController {
+    public class SocketState {
+        private Socket socket;
+        private int ID;
+        private byte[] buffer;
+        private StringBuilder builder;
+        private Delegate callMe;
+
+        public SocketState(Socket s, int id, Delegate d) {
+            Socket = s;
+            ID = id;
+            callMe = d;
+        }
+
+        public Socket Socket { get => socket; }
+        public StringBuilder Builder { get => builder; }
+        public byte[] Buffer { get => buffer; set => buffer = value; }
+    }
+
+    public static class Network {
+        Socket ConnectToServer(Delegate callMe, string hostName) {
+            Socket socket;
+            IPAddress ipAddress;
+            MakeSocket(hostName, out socket, out ipAddress);
+
+            SocketState state = new SocketState(socket, -1, callMe);
+
+            socket.BeginConnect(ipAddress, DEFAULT_PORT, ConnectedCallback, state);
+
+            return socket;
+        }
+
+        void ConnectedCallback(IAsyncResult stateObject) {
+            SocketState state = (SocketState)stateObject;
+
+        }
+
+        void GetData(SocketState state) {
+
+        }
+
+        private void ReceiveCallback(IAsyncResult stateObject) {
+            SocketState state = (SocketState)stateObject.AsyncState;
+
+            int bytesRead = state.Socket.EndReceive(stateObject);
+
+            // If the socket is still open
+            if (bytesRead > 0) {
+                string theMessage = Encoding.UTF8.GetString(state.Buffer, 0, bytesRead);
+                // Append the received data to the growable buffer.
+                // It may be an incomplete message, so we need to start building it up piece by piece
+                state.Builder.Append(theMessage);
+
+                ProcessMessage(state);
+            }
+
+            // Continue the "event loop" that was started on line 96.
+            // Start listening for more parts of a message, or more new messages
+            state.Socket.BeginReceive(state.messageBuffer, 0, state.messageBuffer.Length, SocketFlags.None, ReceiveCallback, state);
+
+        }
+
+        public const int DEFAULT_PORT = 11000;
+
+        private static void MakeSocket(string hostName, out Socket socket, out IPAddress ipAddress) {
+            ipAddress = IPAddress.None;
+            socket = null;
+
+            try {
+                // Establish the remote endpoint for the socket.
+                IPHostEntry ipHostInfo;
+
+                try {
+                    ipHostInfo = Dns.GetHostEntry(hostName);
+                    bool foundIPV4 = false;
+                    foreach (IPAddress addr in ipHostInfo.AddressList)
+                        if (addr.AddressFamily != AddressFamily.InterNetworkV6) {
+                            foundIPV4 = true;
+                            ipAddress = addr;
+                            break;
+                        }
+                    // Didn't find any IPV4 addresses
+                    if (!foundIPV4) {
+                        System.Diagnostics.Debug.WriteLine("Invalid addres: " + hostName);
+                        throw new ArgumentException("Invalid address");
+                    }
+                }
+                catch (Exception) {
+                    // see if host name is actually an ipaddress, i.e., 155.99.123.456
+                    System.Diagnostics.Debug.WriteLine("using IP");
+                    ipAddress = IPAddress.Parse(hostName);
+                }
+
+                // Create a TCP/IP socket.
+                socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+
+                // Disable Nagle's algorithm - can speed things up for tiny messages, 
+                // such as for a game
+                socket.NoDelay = true;
+            }
+            catch (Exception e) {
+                System.Diagnostics.Debug.WriteLine("Unable to create socket. Error occured: " + e);
+                throw new ArgumentException("Invalid address");
+            }
+        }
+    }
+}
--- SpaceWars/View/Form1.cs
+++ SpaceWars/View/Form1.cs
@@ -8,13 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace View
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace View {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
+        }
+
+        // TODO change this to apply to our GUI
+        private void connectButton_Click(object sender, EventArgs e) {
+            // TODO: This needs better error handling. Left as an exercise.
+            // If the server box is empty, it gives a message, but doesn't allow us to try to reconnect.
+            // It also doesn't handle unreachable addresses.
+            if (serverAddress.Text == "") {
+                MessageBox.Show("Please enter a server address");
+                return;
+            }
+
+
+            // Disable the controls and try to connect
+            connectButton.Enabled = false;
+            serverAddress.Enabled = false;
+
+
+            ConnectToServer(serverAddress.Text);
         }
     }
 }
