49a5619bbf7276e2956403c0d4a22a456b532586
Matt Grayston <mattgrayston@gmail.com>
2017-11-15 13:45:07 -0700

    created star, projectile classes

---
+++ SpaceWars/Model/Projectile.cs
@@ -0,0 +1,53 @@
+﻿using SpaceWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Projectile
+    {
+        /// <summary>
+        /// Unique projectile ID
+        /// </summary>
+        private int proj;
+
+
+        /// <summary>
+        /// Vector2D representing the location
+        /// </summary>
+        private Vector2D loc;
+
+        /// <summary>
+        /// Vector2D representing the orientation
+        /// </summary>
+        private Vector2D dir;
+
+        /// <summary>
+        /// a bool representing if the projectile is active or not
+        /// </summary>
+        private bool alive;
+
+        /// <summary>
+        /// an int representing the ID of the ship that created the projectile.
+        /// </summary>
+        private int owner;
+
+        public Projectile(int proj, int owner)
+        {
+            this.proj = proj;
+            this.owner = owner;
+            this.loc = new Vector2D(); //TODO front location of whever the ship is.
+            this.dir = new Vector2D(); //TODO direction of ship
+            this.alive = false;
+        }
+
+        public int getID { get => proj; }
+        public int getOwner { get => owner; }
+        public Vector2D getLoc { get => loc; }
+        public Vector2D getDir { get => dir; }
+        public bool Alive { get => alive; }
+    }
+}
---
+++ SpaceWars/Model/Star.cs
@@ -0,0 +1,39 @@
+﻿using SpaceWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Star
+    {
+        /// <summary>
+        /// Unique ID
+        /// </summary>
+        private int star;
+
+        /// <summary>
+        /// Vector2D representing the location
+        /// </summary>
+        private Vector2D loc;
+
+        /// <summary>
+        /// a double representing the star's mass. Note that the sample client does not use this information, 
+        /// but you may choose to display stars differently based on their mass.
+        /// </summary>
+        private double mass;
+
+        public Star(int star, double mass)
+        {
+            this.star = star;
+            this.loc = new Vector2D(); //TODO center of screen
+            this.mass = mass;
+        }
+
+        public int getID { get => star; }
+        public Vector2D getLoc { get => loc; }
+        public double getMass { get => mass; }
+    }
+}
