b3647dcf427024b8d74dc8c76d807d450ef366a0
Matt Grayston <mattgrayston@gmail.com>
2017-11-29 22:11:33 -0700

    made server project. Setup of NetworkController class.

---
+++ SpaceWars/Server/NetworkController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    /// <summary>
+    /// Used for the connecting host to provide 
+    /// a callback function.
+    /// </summary>
+    /// <param name="state"></param>
+    public delegate void NetworkAction(SocketState state);
+
+    public class SocketState
+    {
+        private Socket socket;
+        private int ID;
+        private NetworkAction callMe;
+
+        public SocketState(Socket s, int id, NetworkAction d)
+        {
+            socket = s;
+            ID = id;
+            callMe = d;
+        }
+
+        public Socket Socket { get => socket; }
+        public NetworkAction CallMe { get => callMe; set => callMe = value; }
+    }
+    /// <summary>
+    /// Stores any socket state
+    /// </summary>
+    class NetworkController
+    {
+
+    }
+}
