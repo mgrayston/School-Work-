cb9ada45d78cf1de3f06e5b625a7e04218966d62
Matt Grayston <mattgrayston@gmail.com>
2017-11-30 19:29:52 -0700

    first pass of NetworkController class

--- SpaceWars/Server/NetworkController.cs
+++ SpaceWars/Server/NetworkController.cs
@@ -1,40 +1,54 @@
-﻿using System;
+﻿using Controller;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server
 {
-    /// <summary>
-    /// Used for the connecting host to provide 
-    /// a callback function.
-    /// </summary>
-    /// <param name="state"></param>
-    public delegate void NetworkAction(SocketState state);
-
-    public class SocketState
+    public class ConnectionState
     {
-        private Socket socket;
-        private int ID;
         private NetworkAction callMe;
+        private List<SocketState> clients;
+        private TcpListener listener;
 
-        public SocketState(Socket s, int id, NetworkAction d)
+
+        public ConnectionState(NetworkAction callMe)
         {
-            socket = s;
-            ID = id;
-            callMe = d;
+            callMe = this.callMe;
+            clients = new List<SocketState>();
+            listener = null;
         }
-
-        public Socket Socket { get => socket; }
+        public List<SocketState> Buffer { get => clients; }
         public NetworkAction CallMe { get => callMe; set => callMe = value; }
+        public TcpListener Listener { get => listener; set => listener = value; }
     }
-    /// <summary>
-    /// Stores any socket state
-    /// </summary>
-    class NetworkController
+    public class NetworkController
     {
+            public const int DEFAULT_PORT = 11000;
+            private TcpListener listener;
+            private ConnectionState state;
 
+        public void ServerAwaitingClientLoop(NetworkAction callMe)
+            {
+                Console.WriteLine("Server waiting for client");
+                listener = new TcpListener(IPAddress.Any, NetworkController.DEFAULT_PORT);
+                state = new ConnectionState(callMe);
+                listener.Start();
+                state.Listener = listener;
+                state.CallMe = callMe;
+                listener.BeginAcceptSocket(AcceptNewClient, state);
+            }
+
+        private void AcceptNewClient(IAsyncResult ar)
+        {
+            state = (ConnectionState)ar.AsyncState;
+            Socket socket = state.Listener.EndAcceptSocket(ar);
+            SocketState ss = new SocketState(socket, -1, null);
+            ss.CallMe = state.CallMe;
+            ss.CallMe(ss);
+            state.Listener.BeginAcceptSocket(AcceptNewClient, state); //TODO - should this take in AcceptNewClient or HandleNewClient?
+        }
     }
 }
